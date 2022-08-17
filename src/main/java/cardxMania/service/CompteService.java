b1ad7422e6bdcb12a422a615762bb54ee64773b3
Louis <louischatelier21@gmail.com>
2022-08-17 16:03:24 +0200

    Spring compte

---
+++ src/main/java/cardxMania/service/CompteService.java
@@ -0,0 +1,41 @@
+package cardxMania.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import cardxMania.dao.CompteRepository;
+import cardxMania.model.Compte;
+
+@Service
+public class CompteService {
+
+	
+
+	@Autowired
+	private CompteRepository compteRepo;
+	
+
+	public List<Compte> getAll() {
+		return compteRepo.findAll();
+	}
+
+	public Compte getById(Integer id) {
+		return compteRepo.findById(id);
+	}
+
+	public Compte create(Compte compte) {
+		return compteRepo.save(compte);
+	}
+
+	public Compte update(Compte compte) {
+		return compteRepo.save(compte);
+	}
+
+	public void deleteById(Integer id) {
+		compteRepo.deleteById(id);
+	}
+	
+	
+}
