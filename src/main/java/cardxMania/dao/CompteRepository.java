fa387a7f3b4a7f69395c51370d32d87f7d8d68cb
Louis <louischatelier21@gmail.com>
2022-08-17 16:20:35 +0200

    suppression repository

--- src/main/java/cardxMania/dao/CompteRepository.java
+++
@@ -1,9 +0,0 @@
-package cardxMania.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import cardxMania.model.Compte;
-
-public interface CompteRepository extends JpaRepository<Compte, Integer> {
-
-}
