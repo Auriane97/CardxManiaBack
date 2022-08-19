a66ad72e8b1e537c143d6b3716c60b078ec8fe54
Alban <alban.atchon@gmail.com>
2022-08-19 12:44:29 +0200

    requêtes exemplaires

--- src/main/java/cardxMania/dao/IDAOExemplaire.java
+++ src/main/java/cardxMania/dao/IDAOExemplaire.java
@@ -1,24 +1,25 @@
 package cardxMania.dao;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import cardxMania.model.Exemplaire;
 
 public interface IDAOExemplaire extends JpaRepository<Exemplaire,Integer> {
 	
 	@Query("select e from Exemplaire e where e.user.id=:id")
-	public void findAllByUser(Integer id);
+	public List <Exemplaire> findAllByUser(@Param("id") Integer id);
 
-	//ALBAN
+	@Query("select e from Exemplaire e where e.carte.id=:id")
+	public List <Exemplaire> findAllByCarte(@Param("id") Integer id);
 	
+	public List<Exemplaire> findByEtat(String etat);
 	
-	
-	//AURIANNE
-	
-	
-	
-	//LOUIS
-	
-	
+	public List<Exemplaire> findByValeurExemplaire(Integer prix);
+
+	@Query("select e from Exemplaire e where e.enVente=TRUE")
+	public List<Exemplaire> findByEnVente(@Param("TRUE") boolean enVente );
 }
