25d02828b58433b85d6b375fbd7c675e80e26454
Alban <alban.atchon@gmail.com>
2022-08-19 14:35:56 +0200

    repo exemplaire

--- src/main/java/cardxMania/dao/IDAOExemplaire.java
+++ src/main/java/cardxMania/dao/IDAOExemplaire.java
@@ -11,10 +11,10 @@ import cardxMania.model.Exemplaire;
 public interface IDAOExemplaire extends JpaRepository<Exemplaire,Integer> {
 	
 	@Query("select e from Exemplaire e where e.user.id=:id")
-	public List <Exemplaire> findAllByUser(@Param("id") Integer id);
+	public List <Exemplaire> findByUser(@Param("id") Integer id);
 
 	@Query("select e from Exemplaire e where e.carte.id=:id")
-	public List <Exemplaire> findAllByCarte(@Param("id") Integer id);
+	public List <Exemplaire> findByCarte(@Param("id") Integer id);
 	
 	public List<Exemplaire> findByEtat(String etat);
 	
